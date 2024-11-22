594918d5fd4bb2344b6ab0e4ed9a6515457fd15e
Phyhan4top <oyedepo304@gmail.com>
2024-11-22 01:46:10 +0100

    committed

--- app/(auth)/login/layout.tsx
+++ app/(auth)/login/layout.tsx
@@ -2,5 +2,5 @@ import AuthTemplate from '@component/templates/AuthTemplate';
 import React from 'react';
 
 export default function layout({ children }: { children: React.ReactNode }) {
-  return <AuthTemplate title="Admim Login">{children}</AuthTemplate>;
+  return <AuthTemplate title="Admin Login">{children}</AuthTemplate>;
 }
